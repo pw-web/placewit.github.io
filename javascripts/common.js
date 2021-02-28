97f1202dabe3c43760fdd758dc9b5c30cc3356ce
placewit <71317829+placewit@users.noreply.github.com>
2021-02-28 20:54:31 +0530

    Loader

--- javascripts/common.js
+++ javascripts/common.js
@@ -7,3 +7,36 @@ function getParamByName(name) {
 	if (!results[2]) return '';
 	return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
+
+
+
+
+function toggleLoaderByDiv(loaderDiv, elementDiv, loaderShow, dark) {
+    if(loaderShow) {
+        $("#"+elementDiv).hide();
+        if(dark)
+            showDivLoaderDark(loaderDiv);
+        else
+            showDivLoader(loaderDiv);
+    }
+    else {
+        $("#"+loaderDiv).html("");
+        $("#"+elementDiv).show();
+    }
+}
+
+function showDivLoader(divId) {
+    $("#"+divId).html(`<div class="lds-ripple" id="ripple-loader"><div></div><div></div></div>`);
+}
+
+function showDivLoaderDark(divId) {
+    $("#"+divId).html(`<div class="lds-ripple ripple-dark" id="ripple-loader"><div></div><div></div></div>`);
+}
+
+function showFullLoader() {
+    $('#fullLoader').show();
+}
+
+function hideFullLoader() {
+    $('#fullLoader').hide();
+}
